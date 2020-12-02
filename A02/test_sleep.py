d3af74c9c3920c7cc3a1afab623599b91b534a21
Pascal Fares <pascal.fares@cofares.net>
2020-12-02 16:20:36 +0200

    cas windows pour time

--- A02/test_sleep.py
+++ A02/test_sleep.py
@@ -1,5 +1,13 @@
 from time import *
+import os
+'''
+if (os.name == 'nt'):
+    from ctypes import windll #new
+    print('nt!')
+    timeBeginPeriod = windll.winmm.timeBeginPeriod #new
+    timeBeginPeriod(1) #new
+'''
 debut = time_ns()
-sleep(0.01)
+sleep(0.00001)
 fin = time_ns()
 print(f"temps écoulé en nanosecondes= {fin-debut} en seconde {(fin-debut)*1e-9}")
---
+++ A02/thread_ex.py
@@ -0,0 +1,34 @@
+import time
+
+# une procedure qui perd du temps!
+def fonct(nom):
+  print(nom)
+  time.sleep(10)
+  print("fin", nom)
+
+def appel_fonc():
+  debut = time.time()
+  fonct("nfp103")
+  fin = time.time()
+  print(f'durée = {fin-debut}')
+
+def pgm(lafonc):
+  print("Avant appel")
+  lafonc()
+  print("Apres appel")
+
+import threading
+def th_appel_fonc():
+  debut = time.time()
+  # fonct("nfp103") Nous allons remplcer ceci par
+  t = threading.Thread(target=fonct, args=['utc503 avec th'])
+  t.start()
+  fin = time.time()
+  print(f'durée = {fin-debut}')
+  t.join()
+
+print("Sans thread .... ")
+pgm(appel_fonc)
+
+print("avace Thread")
+pgm(th_appel_fonc)
